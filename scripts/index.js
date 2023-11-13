56b9474386a98fb400befe8b053d7f8f9c453877
Oleksandr Harashchenko <alexnorch98@gmail.com>
2023-11-13 18:28:09 +0200

    desktop version

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,20 @@
 const accordions = document.querySelectorAll(".accordion-item");
+const movieBtn = document.querySelector(".movie__btn");
+const movie = document.querySelector(".movie__video");
+
+movieBtn.addEventListener("click", function () {
+  if (movie.paused) {
+    movie.play();
+    movieBtn.style.display = "none";
+  }
+});
+
+movie.addEventListener("click", function () {
+  if (!movie.paused) {
+    movie.pause();
+    movieBtn.style.display = "block";
+  }
+});
 
 function toggleAccordion(item) {
   if (item.classList.contains("active")) {
